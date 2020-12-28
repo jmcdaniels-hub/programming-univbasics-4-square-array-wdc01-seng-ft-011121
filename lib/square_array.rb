54ed62dcfb6dfbe5dfe428795369e998254771ed
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-28 19:44:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,14 @@
 
 numbers = [1, 2, 3]
 new_array = []
+index = 0
 
 
 def square_array(numbers)
+  
   numbers.length do |index|
-    new_array.push(numbers[index] ** 2)
-    index += 1
+  new_array.push(numbers[index] ** 2)
+  index +=1
   end  
 end 
 
