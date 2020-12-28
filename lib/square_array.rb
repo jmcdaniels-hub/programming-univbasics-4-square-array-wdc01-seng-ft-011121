eebb17c4eb34bead7b1ee7b79e12c22c16d08868
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-28 19:58:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 
 numbers = [1, 2, 3]
-new_array = []
-counter = 0
+
 
 def square_array(numbers)
-  
+new_array = []
+counter = 0
   while numbers[counter] do
     new_array.push(numbers[counter] ** 2)
     counter +=1
